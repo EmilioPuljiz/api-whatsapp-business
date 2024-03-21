b43d066412dce9eb7bee4985c0efed3ce59ad980
EmilioPuljiz <EmilioPuljiz@users.noreply.github.com>
2024-03-21 20:59:41 +0000

    Fix styling

--- src/ApiWhatsappBusinessServiceProvider.php
+++ src/ApiWhatsappBusinessServiceProvider.php
@@ -11,19 +11,19 @@ class ApiWhatsappBusinessServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (!class_exists('CreateWhatsappConfigurationsTable')) {
+        if (! class_exists('CreateWhatsappConfigurationsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/2023_04_21_110851_create_whatsapp_configurations_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . 'create_whatsapp_configurations_table.php'),
+                __DIR__.'/../database/migrations/2023_04_21_110851_create_whatsapp_configurations_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'create_whatsapp_configurations_table.php'),
             ]);
         }
 
-        if (!class_exists('CreateWhatsappNotificationsTable')) {
+        if (! class_exists('CreateWhatsappNotificationsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/2024_03_04_161027_create_whatsapp_notification_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . 'create_whatsapp_notification_table.php'),
+                __DIR__.'/../database/migrations/2024_03_04_161027_create_whatsapp_notification_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'create_whatsapp_notification_table.php'),
             ]);
         }
 
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations/', 'migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations/', 'migrations');
     }
 
     /**
